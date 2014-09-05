80c1a70f0c084802d34ff7cbdf098fdae95fa9a7
Megan Shao <MeganShao@wl-208-84.wpa.claremont.edu>
2014-09-04 23:03:35 -0700

    Initial Commit

---
+++ BullsEye/MSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MSAppDelegate.h
+//  BullsEye
+//
+//  Created by Megan Shao on 9/4/14.
+//  Copyright (c) 2014 Megan Shao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
