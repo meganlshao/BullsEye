76060863f0693d027c6b061486740d90a5ddfe0a
Megan Shao <MeganShao@wl-208-84.wpa.claremont.edu>
2014-09-06 01:51:31 -0700

    Added about screen

---
+++ BullsEye/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  BullsEye
+//
+//  Created by Megan Shao on 9/6/14.
+//  Copyright (c) 2014 Megan Shao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+- (IBAction)close;
+
+@end
